5a81333ebd5caca9b670b7b71039bfefa3075ee2
shadow25082000 <shadow25082000@gmail.com>
2024-10-07 19:20:25 +0000

    init

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyDZMdiYzYwBssjT7N9-R7CR5uU8ma-1y_c',
+      authDomain: 'sample-firebase-ai-app-1717f.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'sample-firebase-ai-app-1717f',
+      storageBucket: 'sample-firebase-ai-app-1717f.appspot.com',
+      messagingSenderId: '884496950343',
+      appId: '1:884496950343:web:42de8bf01718277dfc3d62',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyCZuyvncVexEPbK_Q2Zr0ZKf76vfMtW0l0',
+  debug_token: ''
+};
